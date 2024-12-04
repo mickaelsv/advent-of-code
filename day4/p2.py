7a60fadfa910e887f52d4815685821c9262d9d75
mickaelsv <mickaelsv14@gmail.com>
2024-12-04 23:16:33 +0100

    corrected the code to make it better

--- day4/p2.py
+++ day4/p2.py
@@ -1,67 +1,38 @@
-count = 0
-
-final_count = 0
-
-
 def check_if_in_grid(i,j,direction,sizeI,sizeJ):
     return ((1<= j < sizeJ-1) and (1<= i < sizeI-1))
 
 def getword(i,j,matrix,direction):
-    # considering the "S" is the arrow of compass
-    north = [j+1,j,j-1]
-    east = [i-1,i,i+1]
-    south = [j-1,j,j+1]
-    west = [i+1,i,i-1]
-    if direction=="northeast":
-        index_list_i = east
-        index_list_j = north
-    elif direction=="southeast":
-        index_list_i = east
-        index_list_j = south
-    elif direction=="southwest":
-        index_list_i = west
-        index_list_j = south
-    elif direction=="northwest":
-        index_list_i = west
-        index_list_j = north
+    index_list_i = [i-1,i,i+1]
+    if direction=="diag1":
+        index_list_j = [j+1,j,j-1]
+    elif direction=="diag2":
+        index_list_j = [j-1,j,j+1]
     return "".join([matrix[index_list_i[index]][index_list_j[index]] for index in range(3)])
-
 
 def check_count(i,j,matrix,direction,sizeI,sizeJ):
     global count
-    if (check_if_in_grid(i,j,direction,sizeI,sizeJ)):
+    if (matrix[i][j] == "A" and check_if_in_grid(i,j,direction,sizeI,sizeJ)):
         word = getword(i,j,matrix,direction)
-        word_to_check = "MAS"
-        if word==word_to_check:
-            count+=1
-
-
-
+        if "M" in word and "S" in word:
+            return 1
+    return 0
 
 def count_santa(matrix):
-    global count
-    global final_count
+    
+    final_count = 0 
     sizeI = len(matrix)
     sizeJ= len(matrix[0])
     for i in range(sizeI):
         for j in range(sizeJ):
+            count_during_iteration = 0
 
-            # North-East
-            check_count(i,j,matrix,"northeast",sizeI,sizeJ)
+            count_during_iteration+=check_count(i,j,matrix,"diag1",sizeI,sizeJ)
 
-            # South-East
-            check_count(i,j,matrix,"southeast",sizeI,sizeJ)    
+            count_during_iteration+=check_count(i,j,matrix,"diag2",sizeI,sizeJ)    
 
-            # South-West
-            check_count(i,j,matrix,"southwest",sizeI,sizeJ)
-
-            # North-West
-            check_count(i,j,matrix,"northwest",sizeI,sizeJ)
-
-            # We need to add a count only if we got 2 check, for the 2 diagonals
-            if(count==2):
+            # We need to add a count only both diagonals are corrects
+            if(count_during_iteration==2):
                 final_count+=1
-            count = 0
 
     return final_count
 
