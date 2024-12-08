7d6507fb67529c100ffc7e0ab01512a290bfa989
mickaelsv <mickaelsv14@gmail.com>
2024-12-08 15:29:44 +0100

    modified day 2 by adding comments

--- day2/p1.py
+++ day2/p1.py
@@ -1,11 +1,20 @@
+### part 1 of the day 2 of adventofcode
+### https://adventofcode.com/2024/day/2
+
+# list of conditions to check
+
+# if your difference is too high
 def difference_too_high(a,b):
     return (abs(a-b)>3)
 
+# if you're not changing
 def not_changing(a,b):
     return a==b
 
+# if you're only going up or going down
 def same_evolution(a,b,statement):
     return statement == (a-b>0)
+
 def main():
     matrix=[]
     file = open("input", "r")
@@ -14,32 +23,27 @@ def main():
         matrix.append(lines.split())
     count = 0
     for lines in matrix:
-        print("len",len(lines))
         lines[0] = int(lines[0])
         valid = True
+
         for i in range(1,len(lines)):
-            print("i",i)
             lines[i] = int(lines[i])
+            # if you are checking first line, you set your statement
             if (i==1):
                 if (difference_too_high(lines[0],lines[1]) or not_changing(lines[0],lines[1])):
                     valid = False
                     continue
                 else:
                     increasing = ((lines[1]-lines[0])>0)
-                    print("this line is increasing ? ",increasing)
+            # else, check if the conditions are corrects
             else:
                 if (difference_too_high(lines[i-1],lines[i]) or not_changing(lines[i-1],lines[i]) or not (same_evolution(lines[i],lines[i-1],increasing)) ):
-                    print("line issue later",lines)
                     if (increasing != ((lines[i]-lines[i-1])>0)):
-                        print("increasing != lines[i]-lines[i-1])>0: ",(lines[i]-lines[i-1])>0)
-                        print("increasing",increasing)
-                        print(increasing != (lines[i]-lines[i-1])>0)
                     valid = False
                     continue
-
+        # if your whole line is valid
         if (valid):
             count+=1
-        print(lines)
     print(count)
 
 main()
